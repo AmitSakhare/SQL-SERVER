7c429a64b2dc56400852e6a858b20f2e7f497875
AmitSakhare <66629728+AmitSakhare@users.noreply.github.com>
2025-02-06 17:21:42 +0530

    Update SELECT_Query.sql

--- SELECT_Query.sql
+++ SELECT_Query.sql
@@ -1,1 +1,54 @@
 select * from dbo.sales;
+
+
+
+[
+  {
+    "DoCID": "DHOINVGS_2024-1",
+    "V_No": 1,
+    "BillNo": "KPM/2024-25/0001",
+    "Billdate": "2024-04-01T00:00:00+05:30"
+  },
+  {
+    "DoCID": "DHOINVGS_2024-2",
+    "V_No": 2,
+    "BillNo": "KPM/2024-25/0002",
+    "Billdate": "2024-04-01T00:00:00+05:30"
+  },
+  {
+    "DoCID": "DHOINVGS_2024-3",
+    "V_No": 3,
+    "BillNo": "KPM/2024-25/0003",
+    "Billdate": "2024-04-01T00:00:00+05:30"
+  },
+  {
+    "DoCID": "DHOEXPGS_2024-1",
+    "V_No": 1,
+    "BillNo": "EXP/2024-25/0001",
+    "Billdate": "2024-04-02T00:00:00+05:30"
+  },
+  {
+    "DoCID": "DHOEXPGS_2024-2",
+    "V_No": 2,
+    "BillNo": "EXP/2024-25/0002",
+    "Billdate": "2024-04-02T00:00:00+05:30"
+  },
+  {
+    "DoCID": "DHOEXPGS_2024-3",
+    "V_No": 3,
+    "BillNo": "EXP/2024-25/0003",
+    "Billdate": "2024-04-02T00:00:00+05:30"
+  },
+  {
+    "DoCID": "DHOEXPGS_2024-4",
+    "V_No": 4,
+    "BillNo": "EXP/2024-25/0004",
+    "Billdate": "2024-04-02T00:00:00+05:30"
+  },
+  {
+    "DoCID": "DHOINVGS_2024-4",
+    "V_No": 4,
+    "BillNo": "KPM/2024-25/0004",
+    "Billdate": "2024-04-02T00:00:00+05:30"
+  }
+]
